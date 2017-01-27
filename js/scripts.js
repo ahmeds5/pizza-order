1c852869a51507bcaae1891daa21a3330bdfa592
saidahmed <ahmeds5@uw.edu>
2017-01-27 11:26:42 -0800

    add a prototype to find the price of the pizza by implementing an if/else statement. will also add changes to the html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,28 @@
 //Business Logic
+var pizzaToppings= [];
 
-function Pizza(size, topings) {
-  this.size= size;
-  this.toppings= toppings;
+function Pizza(pizzaSize, pizzaToppings) {
+  this.pizzaSize= pizzaSize;
+  this.pizzaToppings= pizzaToppings;
   this.pizzaCost=0;
-  this.toppingsCost= 0;
 }
 
 Pizza.prototype.pizzaCost = function() {
-  this.pizzaCost = this.size + toppingsArray.length;
+  if (this.pizzaSize === 9) {
+    this.pizzaCost += 9;
+  }else if (this.pizzaSize === 12) {
+    this.pizzaCost += 12;
+  }else if (this.pizzaSize === 15) {
+    this.pizzaCost += 15;
+  }else if (this.pizzaSize === 18) {
+    this.pizzaCost +=18
+  }else {
+    alert("Pick a pizza size please");
+  }
+  for (var i = 0; i < this.pizzaToppings.length; i++) {
+    this.pizzaCost = this.pizzaCost + 1;
+  }
 }
-
-
-
 
 
 //User Logic
@@ -20,15 +30,19 @@ $(document).ready(function() {
   $("#pizza").submit(function(event) {
     event.preventDefault();
 
-    var toppingsArray= [];
-    var size= parseInt($("#pizzaSize option:selected").val());
+    var pizzaSize= parseInt($("#pizzaSize option:selected").val());
+    var newPizza = new Pizza(pizzaSize, pizzaToppings);
 
     $("input:checkbox[name=topping]:checked").each(function() {
-      toppingsArray.push(parseInt($(this).val()));
+      pizzaToppings.push($(this).val());
     });
 
-      var newPizza = new Pizza(size, toppingsArray);
-      newPizza.pizzaCost();
+    newPizza.pizzaCost();
+    console.log(newPizza);
+
+    $(".bottom").show();
+    $("#priceOutput").text(newPizza.pizzaCost);
+
 
   });
 });
